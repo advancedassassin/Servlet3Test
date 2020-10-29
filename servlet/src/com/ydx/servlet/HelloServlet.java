1745357d0f75cd8e4fd7b2e41d4a9cd0d1237057
= <932938014@qq.com>
2020-10-29 14:41:11 +0800

    新建一个web项目

---
+++ servlet/src/com/ydx/servlet/HelloServlet.java
@@ -0,0 +1,26 @@
+package com.ydx.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @ClassName HelloServlet
+ * @Description 描述一下该类用途
+ * @Author Duxin Yuan
+ * @Date 2020/10/29 14:09
+ * @Version 1.0
+ **/
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    // 重写doGet方法
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        resp.getWriter().write("hello");
+    }
+}
